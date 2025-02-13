32e87e8cc543910650aa52b2e75c1944fbffb9f1
Charles Martin <charlesmartin14@gmail.com>
2025-02-13 12:43:37 -0800

    Update pildataset.py

--- pytorch/pildataset.py
+++ pytorch/pildataset.py
@@ -7,15 +7,20 @@ class PILDataSet(Dataset):
     def __init__(self, train=True, DS="MNIST"):
         self.train = train
         transform = PILToTensor()
+        # Choose the underlying dataset based on DS (case-insensitive)
         if DS.upper() == "CIFAR10":
             self.dataset = CIFAR10("datasets", train=train, download=True, transform=transform)
         elif DS.upper() == "FASHION":
             self.dataset = FashionMNIST("datasets", train=train, download=True, transform=transform)
-        else:  # Default to MNIST
+        elif DS.upper() == "MNIST":
             self.dataset = MNIST("datasets", train=train, download=True, transform=transform)
-    
+        else:
+            raise ValueError("Unknown dataset: " + DS)
+
     def __getitem__(self, index):
+        # Delegate fetching to the underlying dataset.
         return self.dataset[index]
-    
+
     def __len__(self):
+        # Delegate length to the underlying dataset.
         return len(self.dataset)
